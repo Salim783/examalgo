07aa77c312511b8440b6c859c2bd293c197804c3
salim783 <119826525+Salim783@users.noreply.github.com>
2023-03-13 16:37:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+/*exercice1*/
+
+// a = 12;
+// b = 2;
+// c = a * b; 
+// c = 24;
+
+/*exercice 2*/
+
+// A = "123";
+// B = "12";
+// C = A + B;
+// C = "12312";
+
+/*exercice 3*/
+
+// let tx = prompt('donner un prix');
+// let htt = tx * 1.196;
+// console.log(htt);
+
+/*exercice 4*/
+
+// var prénom = prompt('prénom');
+// var nom = prompt('nom');
+// if (prénom === prénom && nom === nom){
+//     console.log('bonjour ' + prénom + ' votre ' + 'nom' + ' est ' + nom );
+// }
+
+/*exercice 5*/
+
+// var un = prompt('numéros un');
+// var deux = prompt('numéros deux');
+// var result = un * deux;
+// if (result < 0){
+//     alert('nombre négatif');
+// }
+// else {alert('nombre positif')}
+// console.log(result);
+
+/*exercice 6*/
+// function aaa(age){
+// age = prompt('votre age?')
+// if(age< 13){
+//     console.log('action man');
+// }
+// else if(age>=13 && age <=18){
+//     console.log('matrix');
+// }
+// else {console.log('evil dead');}
+// }
+/*exercice 7*/
+
+// function chiffre(hzr){
+//     random = Math.round(Math.random()*10)+1;
+//     hzr = parseInt(prompt('chiffre'));
+
+// while(hzr !== random){
+//     hzr = parseInt(prompt('chiffre'))
+// }
+// console.log('bravo')
+// }
+
+/*exercice 8*/
+
+// function hzr(un){
+//     while(un >= 0){
+//         console.log(un);
+//         un = un - 1;
+//     }
+//     return
+// }
+
+/*exercice 9*/
+
+// function devine(){
+//    let random = Math.round(Math.random()*10)
+//    let qqch = 0
+   
+//    while( qqch !== random){
+//         qqch = parseInt(prompt('devine le nombre'))
+//         if (qqch < random){
+//             console.log('trop petit')
+//         }
+//         if(qqch > random){
+//             console.log('trop grand')
+//         }    
+//     }
+//     console.log('bien joué')
+// }
+
+/*exercice 10*/
+
+// function tab() {
+//     let qqch = [10, 15, 20, 15, 14, 8];
+//     let somme = 0;
+// for(let i = 0; i < qqch.length; i++){
+//     somme += qqch[i];
+// }
+// console.log(somme);
+// } 
+
+/*exercice 11*/
+
+// function note(){
+//     eleve = []
+//     let nombreEleve = prompt("combien d'eleve il y a ?") 
+//     for( i = 0; i < nombreEleve; i++){
+//         user = eleve.unshift(prompt('note'))
+//     }
+//     return eleve
+// }
+
+/*exercice 12*/
+
+// function note(){
+//     eleve = []
+//     let nombreEleve = prompt("combien d'eleve il y a ?") 
+//     for( i = 0; i < nombreEleve; i++){
+//         user = eleve.unshift(prompt('note'))
+//     }
+//     moyenne = eleve.filter(eleve => eleve >= 10)
+//     return moyenne
+// }
+
+/*exercice 13*/
+
